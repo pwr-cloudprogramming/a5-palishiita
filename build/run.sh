38c481249285f9a76e791ce5cce49009212a5368
EC2 Default User <ec2-user@ip-172-31-13-87.ec2.internal>
2024-04-25 07:55:11 +0000

    added some changes

--- build/run.sh
+++ build/run.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 echo "This script should start your application"
-docker run -d -p 8080:8080 backend:latest
-docker run -d -p 4173:4173 frontend:latest
+API_URL="http://169.254.169.254/latest/api"
+TOKEN=`curl -X PUT "$API_URL/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 600"`
+TOKEN_HEADER="X-aws-ec2-metadata-token: $TOKEN"
+METADATA_URL="http://169.254.169.254/latest/meta-data"
+IP_V4=`curl -H "$TOKEN_HEADER" -s $METADATA_URL/public-ipv4`
+
+echo "$IP_V4"
+
+sed -i "s/192.168.100.18/$IP_V4/g" /a5-palishiita/build/.env
+
+docker-compose -f /a5-palishiita/build/compose.yaml up -d
